59b671b7c8748418b94e72addb8afc98d8bd37cf
net25 <doryn1@yandex,ru>
2021-10-31 16:03:39 +0300

    new file

---
+++ Practica_2.py
@@ -0,0 +1,23 @@
+def hash_func(num):
+    new_hash = 0
+    for i, bit in enumerate(bin(num)[2:], start=1):
+        new_hash += int(int(bit) + (i**(i*int(bit))) + int(bit)*i**4)
+    return hex(new_hash).upper()[2:]
+
+
+def main():
+    hashes = {}
+    for num in range(100000):
+        hash = hash_func(num)
+        if hash in hashes.values():
+            return 'Error'
+
+        hashes[num] = hash
+
+    return hashes
+
+
+if __name__ == '__main__':
+    number = int(input('Введите число: '))
+    print(hash_func(number))
+    print(main())
