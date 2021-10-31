5988b4b60fc33a01aa09f407823bca7cf043ff16
net25 <doryn1@yandex,ru>
2021-10-31 19:35:20 +0300

    Update file

--- Laba2.py
+++ Laba2.py
@@ -5,17 +5,14 @@ def main():
     Alisa = []
     Eva = []
 
-    # Расчёт числа Боба и передача Алисе
     Bob.append(g ** 15 % p)
     Alisa.append(g ** 15 % p)
     Eva.append(g ** 15 % p)
 
-    # Расчёт числа Алисф и передача Бобу
     Bob.append(g ** 13 % p)
     Alisa.append(g ** 13 % p)
     Eva.append(g ** 13 % p)
 
-    # Расчёт секретного ключа
     Bob.append(Bob[1] ** 15 % p)
     Alisa.append(Alisa[0] ** 13 % p)
 
